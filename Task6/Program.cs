a58b8e92c42be7c4bc737a6af171cc650c254cc7
Ilfat <ilfat95@mail.ru>
2022-02-06 21:57:47 +0500

    add Task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+/// <summary>
+/// Используя Visual Studio, создайте проект по шаблону Console Application. Требуется: 
+/// Создать класс User, содержащий информацию о пользователе (логин, имя, фамилия, возраст, дата заполнения анкеты). 
+/// Поле дата заполнения анкеты должно быть проинициализировано только один раз (при создании экземпляра данного класса) без возможности его дальнейшего изменения. 
+/// Реализуйте вывод на экран информации о пользователе.
+/// </summary>
+namespace Task6
+{
+    class Program
+    {
+        static void Main()
+        {
+            User user = new User();
+            user.Login = "II";
+            user.Name = "ilfat";
+            user.SurName = "Ishberdin";
+            user.Age = 25;
+            user.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public readonly DateTime CreateDate;
+
+        public User()
+        {
+            CreateDate = DateTime.Now;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Login: {Login}\r\n" +
+                $"Name: {Name}\r\n" +
+                $"Surname: {SurName}\r\n" +
+                $"Age: {Age}\r\n" +
+                $"Date of create: {CreateDate.ToString("G")}");
+        }
+    }
+}
